50844c9a4dec621e9758b85cd1041ca41f9db708
Praveen <rpraveen.engineer@gmail.com>
2024-01-17 22:07:07 +0530

    Targeting Enemies With Sword Ability

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -6,9 +6,27 @@ class_name SwordAbilityController
 
 @onready var timer: Timer = $Timer
 
+const MAX_RANGE: int = 150
 
 func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
+
+
+func get_nearest_enemy_position(player_position: Vector2) -> Vector2:
+	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy") as Array[Node]
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player_position) < pow(MAX_RANGE, 2) 
+	)
+	if enemies.size() == 0:
+		return Vector2.ZERO
+	
+	enemies.sort_custom(func (a: Node2D, b: Node2D):
+		var a_distance: float = a.global_position.distance_squared_to(player_position)
+		var b_distance: float = b.global_position.distance_squared_to(player_position)
+		return a_distance < b_distance
+	)
+	
+	return enemies[0].global_position
 
 
 func spawn_sword_ability() -> void:
@@ -17,7 +35,7 @@ func spawn_sword_ability() -> void:
 	
 	var sword_ability: Node2D = sword_ability_scene.instantiate() as Node2D
 	player.get_parent().add_child(sword_ability)
-	sword_ability.global_position = player.global_position
+	sword_ability.global_position = get_nearest_enemy_position(player.global_position)
 	
 
 func _on_timer_timeout() -> void:
