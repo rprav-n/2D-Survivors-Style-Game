9fae22f52f1553b5c9ccb6f75c2662494793f575
Praveen <rpraveen.engineer@gmail.com>
2024-01-17 22:35:57 +0530

    Destroying Enemies

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -12,31 +12,31 @@ func _ready() -> void:
 	timer.timeout.connect(_on_timer_timeout)
 
 
-func get_nearest_enemy_position(player_position: Vector2) -> Vector2:
-	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy") as Array[Node]
-	enemies = enemies.filter(func(enemy: Node2D):
-		return enemy.global_position.distance_squared_to(player_position) < pow(MAX_RANGE, 2) 
-	)
-	if enemies.size() == 0:
-		return Vector2.ZERO
-	
-	enemies.sort_custom(func (a: Node2D, b: Node2D):
-		var a_distance: float = a.global_position.distance_squared_to(player_position)
-		var b_distance: float = b.global_position.distance_squared_to(player_position)
-		return a_distance < b_distance
-	)
-	
-	return enemies[0].global_position
-
-
 func spawn_sword_ability() -> void:
 	var player: Player = get_tree().get_first_node_in_group("player") as Player	
 	if sword_ability_scene == null || player == null: return
 	
+	var enemies: Array[Node] = get_tree().get_nodes_in_group("enemy") as Array[Node]
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2) 
+	)
+	if enemies.size() == 0:
+		return
+	
+	enemies.sort_custom(func (a: Node2D, b: Node2D):
+		var a_distance: float = a.global_position.distance_squared_to(player.global_position)
+		var b_distance: float = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	var enemy: Node2D = enemies[0] as Node2D
 	var sword_ability: Node2D = sword_ability_scene.instantiate() as Node2D
 	player.get_parent().add_child(sword_ability)
-	sword_ability.global_position = get_nearest_enemy_position(player.global_position)
+	sword_ability.global_position = enemy.global_position
+	sword_ability.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
 	
+	var enemy_direction: Vector2 = enemy.global_position - sword_ability.global_position
+	sword_ability.rotation = enemy_direction.angle()
+
 
 func _on_timer_timeout() -> void:
 	spawn_sword_ability()
--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -4,6 +4,12 @@ class_name BasicEnemy
 
 const MAX_SPEED: int = 75
 
+@onready var area_2d: Area2D = $Area2D
+
+
+func _ready() -> void:
+	area_2d.area_entered.connect(_on_area_entered)
+
 
 func _process(_delta: float) -> void:
 	var direction: Vector2 = get_direction_to_player()
@@ -15,3 +21,7 @@ func get_direction_to_player() -> Vector2:
 	var player: Player = get_tree().get_first_node_in_group("player") as Player
 	if !is_instance_valid(player): return Vector2.ZERO
 	return (player.global_position - global_position).normalized()
+
+
+func _on_area_entered(_other_area2d: Area2D) -> void:
+	queue_free()
