cf4e07ef7851f0a5076a93b50892d7c95589873a
Praveen <rpraveen.engineer@gmail.com>
2024-01-26 12:40:21 +0530

    Improve tilemap

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,10 +3,12 @@ extends Node
 class_name Main
 
 @export var end_screen_scene: PackedScene
+@export var bg_color: Color
 @onready var player: Player = %Player
 
 
 func _ready() -> void:
+	RenderingServer.set_default_clear_color(bg_color)
 	player.health_component.died.connect(_on_player_died)
 
 
