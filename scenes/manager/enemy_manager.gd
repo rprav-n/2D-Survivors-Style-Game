97f8a893efceaa367899da5345b3fe6ecb46d476
Praveen <rpraveen.engineer@gmail.com>
2024-01-26 12:59:23 +0530

    Preventing invalid spawning

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -9,10 +9,32 @@ const SPAWN_RADIUS: int = 375
 var base_spawn_time: float = 0
 @onready var timer: Timer = $Timer
 
+
 func _ready() -> void:
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(_on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(_on_arena_difficulty_increased)
+
+
+func get_spawn_position() -> Vector2:
+	var player: Player = get_tree().get_first_node_in_group("player") as Player
+	if player == null:
+		return Vector2.ZERO
+
+	var random_direction: Vector2 = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var spawn_position: Vector2 = player.global_position + (random_direction * SPAWN_RADIUS)
+
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		var query_paramerters: PhysicsRayQueryParameters2D = 	PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var result: Dictionary = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramerters)
+	
+		if result.is_empty():
+			return spawn_position
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+
+	return spawn_position
 
 
 func _on_timer_timeout() -> void:
@@ -21,13 +43,11 @@ func _on_timer_timeout() -> void:
 	if player == null:
 		return
 
-	var random_direction: Vector2 = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position: Vector2 = player.global_position + (random_direction * SPAWN_RADIUS)
 	
 	var enemy: Node2D = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer: Node = get_tree().get_first_node_in_group("entities_layer") as Node
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func _on_arena_difficulty_increased(arena_difficulty: int) -> void:
