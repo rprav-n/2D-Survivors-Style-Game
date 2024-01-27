8ad41762122ab47539fc07634d779d640d766d9b
Praveen <rpraveen.engineer@gmail.com>
2024-01-27 16:18:42 +0530

    Animating the Upgrade Card

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -6,17 +6,52 @@ signal selected
 
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hover_animation_player: AnimationPlayer = $HoverAnimationPlayer
 
+var disabled: bool = false
 
 func _ready() -> void:
 	gui_input.connect(_on_gui_input)
+	mouse_entered.connect(_on_mouse_entered)
 	
 
 func set_ability_upgrade(upgrade: AbilityUpgrade) -> void:
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+	
+
+func play_in(delay: float = 0) -> void:
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
+
+
+func play_discard() -> void:
+	animation_player.play("discard")
+
+
+func select_card() -> void:
+	disabled = true
+	animation_player.play("selected")
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		(other_card as AbilityUpgradeCard).play_discard()
+		
+	await animation_player.animation_finished
+	selected.emit()
 
 
 func _on_gui_input(event: InputEvent) -> void:
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+
+func _on_mouse_entered() -> void:
+	if disabled:
+		return
+	hover_animation_player.play("hover")
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -13,11 +13,14 @@ func _ready() -> void:
 
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]) -> void:
+	var delay: float = 0
 	for upgrade in upgrades:
 		var ability_upgrade_card: AbilityUpgradeCard = ability_upgrade_card_scene.instantiate() as AbilityUpgradeCard
 		card_container.add_child(ability_upgrade_card)
 		ability_upgrade_card.set_ability_upgrade(upgrade as AbilityUpgrade)
+		ability_upgrade_card.play_in(delay)
 		ability_upgrade_card.selected.connect(_on_selected.bind(upgrade))
+		delay += 0.2
 		
 		
 func _on_selected(upgrade: AbilityUpgrade) -> void:
