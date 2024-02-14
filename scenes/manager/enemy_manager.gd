8720bdf85260036a4e91eff61b7a603150ae47ff
Praveen <rpraveen.engineer@gmail.com>
2024-02-15 00:24:39 +0530

    fix enemy spawn

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -30,7 +30,9 @@ func get_spawn_position() -> Vector2:
 
 	for i in 4:
 		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
-		var query_paramerters: PhysicsRayQueryParameters2D = 	PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var additional_check_offset = random_direction * 20
+		
+		var query_paramerters: PhysicsRayQueryParameters2D = 	PhysicsRayQueryParameters2D.create(player.global_position, spawn_position + additional_check_offset, 1 << 0)
 		var result: Dictionary = get_tree().root.world_2d.direct_space_state.intersect_ray(query_paramerters)
 	
 		if result.is_empty():
