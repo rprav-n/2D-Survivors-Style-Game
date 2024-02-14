5bdc2869e123b7b027f4676af7246a99747da9f6
Praveen <rpraveen.engineer@gmail.com>
2024-02-15 00:45:26 +0530

    complete

--- scenes/ui/options_menu.gd
+++ scenes/ui/options_menu.gd
@@ -45,6 +45,7 @@ func _on_window_button_pressed() -> void:
 	if mode != DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	update_display()
 
